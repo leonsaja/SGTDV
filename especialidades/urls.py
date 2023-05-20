eaf3debe384d7f85e271141aa3f5433407122001
leonsaja <leonardosaja87@gamil.com>
2023-05-20 16:25:00 -0300

    teste

--- especialidades/urls.py
+++ especialidades/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from .views import tipo_especialidades_view as view_tipoespecialidade
 from .views import especialidade_view as view_especialidade
+from .views import tipo_especialidades_view as view_tipoespecialidade
 
 app_name='especialidades'
 
@@ -15,7 +15,7 @@ urlpatterns = [
 
 
     #Especialidade
-    path('create/<int:id>/paciente_especialidade/', view_especialidade.especialidadeCreate, name='add-paciente-especialidade' ),  
+    path('<int:id>/create/  paciente_especialidade/', view_especialidade.especialidadeCreate, name='add-paciente-especialidade' ),  
     path('update/<int:id>/paciente_especialidade/', view_especialidade.especialidadeUpdate, name='edit-paciente-especialidade' ),  
     path('list/<int:pk>/pacientes_especialidades/', view_especialidade.EspecialidadeListView.as_view(), name='list-paciente-especialidade' ),
     path('detail/<int:pk>/paciente_especialidade/', view_especialidade.EspecialidadeDetailView.as_view(), name='detail-paciente-especialidade' ),  
