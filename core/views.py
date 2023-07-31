f6d63ace75d441d14c6b853d96f0638a575cd333
leonsaja <leonardosaja87@gmail.com>
2023-07-31 18:15:41 -0300

    modificando dashboard

--- core/views.py
+++ core/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from cidadao.models import Cidadao
+from despesas.models import Diaria
+from transportes.models import Viagem
+from tfds.models import ReciboTFD
 
 
 def home(request):
-    return render(request,'home.html')
+   context={}
+   context['qta_cidadao']=Cidadao.objects.count()
+   context['qta_diaria']=Diaria.objects.count()
+   context['qta_viagens']=Viagem.objects.count()
+   context['qta_recibo_tfd']=ReciboTFD.objects.count()
+
+   return render(request,'home.html',context)
