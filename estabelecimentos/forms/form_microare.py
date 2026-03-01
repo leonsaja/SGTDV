13c72d75b6d093e441cdd510609d9427bb7ea0b7
leonsaja <leonardosaja87@gmail.com>
2026-03-01 17:19:03 -0300

    correcao app

--- estabelecimentos/forms/form_microare.py
+++ estabelecimentos/forms/form_microare.py
@@ -1,10 +1,15 @@
 from django import forms
 
 from ..models import MicroArea
-
+from profissionais.models import Profissional
 
 class MicroAreaForm(forms.ModelForm):
     
     class Meta:
         model=MicroArea
         fields='__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        acs = Profissional.objects.select_related('estabelecimento').filter(cargo=1)
+        self.fields['profissional'].queryset = acs
